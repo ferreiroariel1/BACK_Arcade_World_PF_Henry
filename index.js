158b1fc217e368395b587b82a45c1d22431990f3
Ariel Ferreiro <ferreiroariel1@gmail.com>
2023-11-07 03:29:19 -0300

    vercel deploy

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const server = require('./src/app.js') 
 const { conn } = require('./src/db.js')
 
+const port = process.env.PORT || 3001
+
 conn.sync({alter:  true})
 .then(() => {
     server.listen(3001, () => {
-        console.log('App listening on port 3001!');
+        console.log(`App listening on port ${port}!`);
     })
 })
